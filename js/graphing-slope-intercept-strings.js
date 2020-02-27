e4ae5f8e8d3bd8e66d75162b05267926a0bb2ab0
samreid <reids@colorado.edu>
2020-02-27 12:07:56 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/graphing-slope-intercept-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import graphingSlopeIntercept from './graphingSlopeIntercept.js';
+
+const graphingSlopeInterceptStrings = getStringModule( 'GRAPHING_SLOPE_INTERCEPT' );
+
+graphingSlopeIntercept.register( 'graphingSlopeInterceptStrings', graphingSlopeInterceptStrings );
+
+export default graphingSlopeInterceptStrings;
