4d3dbaee40e8374f7d823618a0ae86cfcd23011a
pixelzoom <cmalley@pixelzoom.com>
2024-02-08 00:52:37 -0700

    Revert "move portrayals to view/portrayals/, https://github.com/phetsims/graphing-lines/issues/143"
    This reverts commit d020cd4ed407d0e3d99428270e8865a11418b431.

---
+++ js/linegame/view/ClimberPortrayalAsia.ts
@@ -0,0 +1,23 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * This file instantiates the Asia region and culture portrayals.
+ *
+ * @author Luisa Vargas
+ */
+
+import ClimberPortrayal from '../../../../graphing-lines/js/linegame/view/ClimberPortrayal.js';
+import JoistStrings from '../../../../joist/js/JoistStrings.js';
+import { ASIA_REGION_AND_CULTURE_ID } from '../../../../joist/js/preferences/RegionAndCulturePortrayal.js';
+import level1_svg from '../../../../graphing-lines/images/asia/level1_svg.js';
+import level3_svg from '../../../../graphing-lines/images/asia/level3_svg.js';
+import level5_svg from '../../../../graphing-lines/images/asia/level5_svg.js';
+import level6_svg from '../../../../graphing-lines/images/asia/level6_svg.js';
+
+const ClimberPortrayalAsia = new ClimberPortrayal(
+  JoistStrings.preferences.tabs.localization.regionAndCulture.portrayalSets.asiaStringProperty,
+  ASIA_REGION_AND_CULTURE_ID,
+  [ level1_svg, level3_svg, level5_svg, level6_svg ]
+);
+
+export default ClimberPortrayalAsia;
